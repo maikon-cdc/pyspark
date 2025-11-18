38f514b122a1742e35aa0c71beadd6f56a539173
maikon <raphaelmartins1305@gmail.com>
2025-11-17 21:09:28 -0300

    new

---
+++ atividade_lista.py
@@ -0,0 +1,20 @@
+# criar uma lista com três números: 10, 20 e 30
+# exiba todos eles na tela
+# tente exibir separadamente
+
+lista = ["10", "20", "30"]
+print(lista)
+print(lista[0])
+print(lista[1])
+print(lista[2])
+
+# crie uma lista vazia e peça ao usuário 3 números, cada múmero digitado deve ser adicionado á lista
+# usando a função append()
+
+lista = []
+for in range(3):
+    valor = int(input"aléatorio")
+    lista.append(valor)
+    print(lista)
+
+# escreva um código que peça a identidade e o nome do usuário e imprima o nome e identidade 
